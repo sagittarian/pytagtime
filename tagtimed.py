c3dd6011d98accd6c95cfa7722f2acc30b8009c2
Adam Mesha <adam@mesha.org>
2014-07-23 21:51:10 +0300

    Start of work

---
+++ tagtimed.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+'''
+  TagTime daemon: this figures out from scratch when to beep and does so,
+    continuously, even when the previous ping is still unanswered.
+  After each ping it also runs launch.pl (with the 'quiet' arg since
+    this is already doing the beeping) which launches popups or an editor
+    for any overdue pings.
+  Might be nice to watch ~/.tagtimerc (aka settings.pl) for changes a la
+    watching.pl so that we don't have to restart this daemon when settings
+    change. (does it suffice to just re-require it?)
+'''
+#
+#=head1 NAME
+#
+#tagtimed - Stochastic tagging of your time!
+#
+#=head1 SYNPOSIS
+#
+#    $ tagtimed.pl     # Start tagtime
+#
+#    $ tagtimed.pl -l  # Start tagtime if not already running.
+#
+#    $ tagtimed.pl -s  # Start tagtime if not already running, be silent otherwise.
+#
+#=head1 DESCRIPTION
+#
+#See L<http://messymatters.com/tagtime/>
+#
+#You can add the following to your C<.bashrc> or equivalent to make sure TagTime starts
+#when you open a shell:
+#
+#    tagtimed.pl -l -s &
+#
+#=head1 BUGS
+#
+#The C<-l> switch must be used on all processes for locks to work correctly.
+#
+#The C<-l> switch will exit Tagtime if it's already running with another user using the C<-l>
+#switch.
+#
+#=cut
+#
+#{
+#    use strict;
+#    use warnings;
+#    use Fcntl qw(:flock);
+#    use Getopt::Std qw(getopts);
+#
+#    # Lock our process if required.  For more details, see:
+#    # http://perltraining.com.au/tips/2005-08-08.html
+#
+#    # Note: without the '-l' switch, we won't even try to create a lock.
+#
+#    my %opts = ( l => 0, s => 0);   # [l]ock and [s]ilent
+#
+#    getopts('ls',\%opts);
+#
+#    # Lock process if called with -l switch
+#    if ($opts{l} and not flock(DATA,LOCK_EX|LOCK_NB)) {
+#
+#        # Oops, we're already locked...
+#        exit(0) if $opts{s};        # Silent exit on already locked.
+#        die "Already running\n";    # Noisy exit otherwise.
+#    }
+#}
+#
+#$launchTime = time();
+
+import os
+import time
+
+
+def callcmd(cmd):
+    if subprocess.call(cmd) != 0:
+        print('SYSERR:', cmd, file=sys.stderr)
+
+launchtime = time.time()
+SETTINGS_FILE = os.path.expanduser('~/.tagtimerc')
+
+class Settings:
+    def __init__(self, settingsfile=SETTINGS_FILE):
+        self.settingsfile = SETTINGS_FILE
+
+settings = Settings()
+
+from util import *
+
+lstping = util.prevping(launchtime)
+nxtping = util.nextping(lstping)
+
+
+# XXX locking
+#if($cygwin) { unlock(); }  # on cygwin may have stray lock files around.
+#
+#$cmd = "${path}launch.pl";        # Catch up on any old pings.
+cmd = os.path.join(path, 'launch.py')
+if subprocess.call(cmd) != 0:
+    print('SYSERR:', cmd, file=sys.stderr)
+
+print("TagTime is watching you! Last ping would've been", ss(time.time()-lstping), "ago.";, file=sys.stderr)
+
+start = time.time()
+i = 1
+
+while True:
+#while(1) {
+  # sleep till next ping but check again in at most a few seconds in
+  # case computer was off (should be event-based and check upon wake).
+  time.sleep(clip(nxtping - time.time(), 0, 2));
+  now = time.time()
+
+  if nxtping <= now:
+      if catchup or nxtping > now - retrothresh:
+          if not playsound:
+              print('\a', file=sys.stderr)
+          else:
+              callcmd(playsound)
+
+    # invokes popup for this ping plus additional popups if there were more
+    #   pings while answering this one:
+    cmd = [os.path.join(path, 'launch.py'), 'quiet', '&'];
+    callcmd(cmd)
+#    print STDERR annotime(padl($i," ",4).": PING! gap ".
+#			  ss($nxtping-$lstping)."  avg ".
+#                          ss((0.0+time()-$start)/$i). " tot ".
+#                          ss(0.0+time()-$start), $nxtping, 72), "\n";
+#    $lstping = $nxtping;
+#    $nxtping = nextping($nxtping);
+#    $i++;
+#  }
+#}
+#
+## Invoke popup for this ping plus additional popups if there were more pings
+## while answering this one.
+#sub pingery {
+#  # TODO: move everything from launch.pl to here
+#  return 0;
+#}
+#
+##SCHDEL (scheduled for deletion):
+##check if X11 is already running, and if not, start it
+##$X11= '/Applications/Utilities/X11.app/Contents/MacOS/X11 &';
+##if (-e $X11) {
+##	$filename='/tmp/.X0-lock';
+##	my $xorg=`ps -A|grep -c 'X11.app'`;
+##	print $xorg;
+##	#unless (-e $filename || $xorg>1) {
+##	unless ($xorg>2) {
+##		`$X11`
+##	}
+##}
+#
+#__DATA__
+#This section exists to make it trivial to implement the -l (lock)
+#feature of tagtimed.
+#
---
+++ util.py
@@ -0,0 +1,479 @@
+# Utility functions for tagtime.
+# This uses settings from ~/.tagtimerc so that must have been loaded first. # XXX
+
+import settings # XXX tagtime settings
+
+import math
+import re
+import subprocess
+import sys
+
+def clip(x, a, b):
+    return max(a, min(b, x))
+
+
+class Random:
+
+    def __init__(self, gap, seed=666):
+        self.IA = 16807      # constant used for RNG
+                             # (see p37 of Simulation by Ross)
+        self.IM = 2147483647 # constant used for RNG (2^31-1)
+        self.seed = seed     # state of the RNG
+        self.initseed = seed
+        self.gap = gap
+
+    def ran0(self):
+        '''Returns a random integer in [1,$IM-1]; changes self.seed, ie, RNG
+        state.  (This is ran0 from Numerical Recipes and has a period
+        of ~2 billion.)
+
+        '''
+        self.seed = self.IA * self.seed % self.IM
+        return self.seed
+
+    def ran01(self):
+        '''Returns a U(0,1) random number.'''
+        return self.ran0() / self.IM
+
+
+    def exprand(self):
+        '''Returns a random number drawn from an exponential distribution with mean
+        self.gap.
+
+        '''
+        return -1 * self.gap * math.log(self.ran01())
+
+    def nextping(self, prev):
+        '''Takes previous ping time, returns random next ping time (unixtime).
+        NB: this has the side effect of changing the RNG state ($seed)
+            and so should only be called once per next ping to calculate,
+            after calling prevping.
+        '''
+        # XXX round1
+        return max(prev + 1, round1(prev + self.exprand()))
+
+    def prevping(self, t):
+        '''Computes the last scheduled ping time before time t.'''
+        self.seed = self.initseed
+        # Starting at the beginning of time, walk forward computing
+        # next pings until the next ping is >= t.
+        nxtping = 1184083200  # the birth of timepie/tagtime!
+        lstping = nxtping
+        lstseed = seed
+        while nxtping < t:
+            lstping = nxtping
+            lstseed = self.seed # XXX this is an ugly way to handle state
+            nxtping = self.nextping(nxtping)
+        self.seed = lstseed
+        return lstping
+
+
+def strip(s):
+    '''Strips out stuff in parens and brackets;
+    remaining parens/brackets means they were unmatched.
+
+    '''
+    while True:
+        s, n = re.subn(r'\([^\(\)]*\)', '', s)
+        if n == 0:
+            break
+    while True:
+        s, n = re.subn(r'\[[^\[\]]*\]', '', s)
+        if n == 0:
+            break
+    # Also remove trailing whitespace? (this breaks cntpings)
+    # s = re.sub(r'\s*$', '', s)
+
+    return s
+
+def stripb(s):
+    '''Strips out stuff in brackets only; remaining brackets means
+    they were unmatched.
+
+    '''
+    return re.sub(r'\s*\[[^\[\]]*\]', '', s)
+
+
+def stripc(s):
+    '''Strips out stuff *not* in parens and brackets.'''
+    pairs = {
+        '(': ')',
+        '[': ']'
+    }
+    reverse_pairs = {pairs[val]: key for key in pairs}
+
+    result = []
+    openers = []
+    i = 0
+    while i < len(s):
+        if s[i] in pairs:
+            openers.append(i)
+        elif s[i] in reverse_pairs:
+            if openers and s[openers[-1]] == reverse_pairs[s[i]]:
+                start = openers.pop()
+                if not openers:  # closed all parens
+                    result.append(s[start:i+1])
+                # else ignore/continue, will be appended to the
+                # result when the last parens is closed
+            # else unmatched closer, ignore
+        i += 1
+
+    # Here is the original perl implementation
+    # sub stripc {
+    #   my($s) = @_;
+    #   my $tmp = $s;
+    #   while($tmp =~ s/\([^\(\)]*\)/UNIQUE78DIV/g) {}
+    #   while($tmp =~ s/\[[^\[\]]*\]/UNIQUE78DIV/g) {}
+    #   my @a = split('UNIQUE78DIV', $tmp);
+    #   for(@a) {
+    #     my $i = index($s, $_);
+    #     substr($s, $i, length($_)) = "";
+    #   }
+    #   return $s;
+    # }
+
+def parsable(s):
+    '''Whether the given string is valid line in a tagtime log file'''
+    s = self.strip(s)
+    # return not (not re.search(r'^\d+\s+', s) or
+    #             re.search(r'(\(|\)|\[|\])', s))
+    return re.search(r'^\d+\s+', s) and not re.search(r'(\(|\)|\[|\])', s)
+
+def fetchp(s):
+    '''Fetches stuff in parens. Not currently used.'''
+    tmp = stripc(s)
+    return NotImplemented
+    #sub fetchp {
+    #  my($s) = @_;
+    #  my $tmp = $s;
+    #  while($tmp =~ s/\([^\(\)]*\)/UNIQUE78DIV/g) {}
+    #  my @a = split('UNIQUE78DIV', $tmp);
+    #  for(@a) {
+    #    my $i = index($s, $_);
+    #    substr($s, $i, length($_)) = "";
+    #  }
+    #  $s =~ s/^\(//;
+    #  $s =~ s/\)$//;
+    #  return $s;
+    #}
+
+def gettags(s):
+    '''Extracts tags prepended with colons and returns them space-separated.
+    Eg: "blah blah :foo blah :bar" --> "foo bar"'''
+    return re.findall(r'\s\:([\w\_]+)', strip(s))
+
+def lockb():
+    '''Blocking lock -- try to get the lock and wait if we can't.'''
+    okFlag = True # False if we had to override the lock or something.
+    # okFlag is currently just "whether we had to wait at all for the lock"
+    if cygwin: # stupid windows # XXX cygwin
+        while os.path.exists(lockf): # XXX lockf
+            print("TagTime is locked.  Waiting 30 seconds...\n")
+            time.sleep(30)
+            okFlag = False
+        cmd = ["/usr/bin/touch", lockf]
+        if subprocess.call(cmd) != 0:
+            print("SYSERR:", cmd, file=sys.stderr)
+    else: # nice unix (including mac)
+        #  my $okFlag = 1;
+        #  if($cygwin) {
+        #    while(-e $lockf) {
+
+        #      sleep(30);
+        #      $okFlag = 0;
+        #    }
+        #    $cmd = "/usr/bin/touch $lockf";
+        #    system($cmd) == 0 or print "SYSERR: $cmd\n";
+        #  } else {
+        #    sysopen(LF, $lockf, O_RDONLY | O_CREAT) or die "Can't open lock file: $!";
+        with open(lockf, 'r') as lf:
+            try:
+                fcntl.lockf(lf, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            except IOError as e:
+                if e.errno in (errno.EACCES, errno.EAGAIN):
+                    print('Tagtime is locked.  Waiting...',
+                          end=' ', file=sys.stderr)
+                    try:
+                        fcntl.lockf(lf, LOCK_EX)
+                    except IOError as e2:
+                        print("Can't lock {}: {}".format(lockf, e2))
+                        raise
+                    print('ready!', end='\n\n')
+                    okFlag = False
+        return okFlag
+        #    if(!flock(LF, LOCK_EX | LOCK_NB)) {  # exclusive, nonblocking lock.
+        #      print "TagTime is locked.  Waiting...";
+        #      flock(LF, LOCK_EX) or die "Can't lock $lockf: $!";
+        #      print " ready!\n\n";
+        #      $okFlag = 0;
+        #    }
+
+
+def lockn():
+    '''Nonblocking lock -- try to get the lock and return 0 if we can't.'''
+    if settings.cygwin:  # stupid windows
+        if os.path.exists(lockf): return False
+        cmd = ["/usr/bin/touch", lockf]
+        if subprocess.call(cmd) != 0:
+            print("SYSERR:", cmd, file=sys.stderr)
+    else:
+        pass
+#sub lockn {
+#  if($cygwin) {  # stupid windows
+#    if(-e $lockf) { return 0; }
+#    $cmd = "/usr/bin/touch $lockf";
+#    system($cmd) == 0 or print "SYSERR: $cmd\n";
+#  } else {  # nice unix (including mac)
+#    sysopen(LF, $lockf, O_RDONLY | O_CREAT) or die "Can't open lock file: $!";
+#    # Don't wait if we can't get the lock, the next cron'd version'll get it
+#    if(!flock(LF, LOCK_EX | LOCK_NB)) { return 0; }
+#    flock(LF, LOCK_EX) or die "Can't lock $lockf: $!";
+#  }
+#  return 1;
+#}
+#
+## Release the lock.
+#sub unlock {
+#  if($cygwin) {  # stupid windows
+#    $cmd = "/bin/rm -f $lockf";
+#    system($cmd) == 0 or print "SYSERR: $cmd\n";
+#  } else {  # nice unix
+#    close(LF);  # release the lock.
+#  }
+#}
+#
+def splur(n, noun):
+    '''Singular or Plural:  Pluralize the given noun properly, if n is not 1.
+    Eg: splur(3, "boy") -> "3 boys"'''
+    return '{n} {noun}{end}'.format(
+        n=n, noun=noun, end='' if n == 1 else 's')
+
+
+## Trim whitespace from front and back of string s.
+#sub trim { my($s) = @_;  $s =~ s/^\s+//;  $s =~ s/\s+$//;  return $s; }
+
+
+##
+#sub divider { my($label) = @_;
+#  #if(!defined($linelen)) { $linelen = 79; }
+#  my $n = length($label);
+#  my $left = int(($linelen - $n)/2);
+#  my $rt = $linelen - $left - $n;
+#  return ("-"x$left).$label.("-"x$rt);
+#}
+def divider(label):
+    '''Takes a string "foo" and returns "-----foo-----" of length $linelen.'''
+    n = len(label)
+    # XXX
+#
+## Takes 2 strings and returns them concatenated with enough space in the middle
+## so the whole string is $x long (default: $linelen).
+#sub lrjust { my($a, $b, $x) = @_;
+#  $x = $linelen unless defined($x);
+#  "$a " . " "x(max(0,$x-length("$a $b"))) . $b;
+#}
+#
+
+def annotime(a, t, ll):
+    '''Annotates a line of text with the given timestamp.'''
+    # XXX make it the same
+    ts = datetime.datetime.fromtimestamp(t).isoformat()
+    return '{} {}'.format(a, ts)
+    #sub annotime {                 # NB: this does not include a newline.
+    #  my($a, $t, $ll) = @_;
+    #  $ll = $linelen unless defined($ll);
+    #  my($yea,$o,$d,$h,$m,$s,$wd) = dt($t);
+    #  my @candidates = (
+    #    #"[$yea.$o.$d $h:$m:$s $wd; r=".round1(time()-$t)."]",
+    #    "[$yea.$o.$d $h:$m:$s $wd]",    # 24 chars
+    #    "[$o.$d $h:$m:$s $wd]",         # 18 chars
+    #    "[$d $h:$m:$s $wd]",            # 15 chars
+    #    "[$o.$d $h:$m:$s]",             # 14 chars
+    #    "[$h:$m:$s $wd]",               # 12 chars
+    #    "[$o.$d $h:$m]",                # 11 chars
+    #    "[$d $h:$m:$s]",                # also 11 so this will never get chosen
+    #    "[$h:$m $wd]",                  #  9 chars
+    #    "[$h:$m:$s]",                   #  8 chars
+    #    "[$d $h:$m]",                   # also 8 so this will never get chosen
+    #    "[$h:$m]",                      #  5 chars
+    #    "[$m]"                          #  2 chars
+    #  );
+    #  for(@candidates) {
+    #    if(length("$a $_") <= $ll) {
+    #      return lrjust($a, $_, $ll-0*24);
+    #    }
+    #  }
+    #  return $a;
+    #}
+
+class Logger:
+
+    def __init__(self, logf, linelen = 80):
+        self.logf = logf
+        self.linelen = linelen
+
+    def log(self, s):
+        '''append a string to the log file ($logf defined in settings file)'''
+        with open(self.logf, 'a') as f:
+            f.write(s)
+    slog = log # original name
+
+def dd(n):
+    '''double-digit: takes number from 0-99,
+    returns 2-char string eg "03" or "42".'''
+    return '{:02}'.format(n)
+
+
+
+## pad left: returns string x but with p's prepended so it has width w
+#sub padl {
+#  my($x,$p,$w) = @_;
+#  if(length($x) >= $w) { return substr($x,0,$w); }
+#  return $p x ($w-length($x)) . $x;
+#}
+#
+## pad right: returns string x but with p's appended so it has width w
+#sub padr {
+#  my($x,$p,$w) = @_;
+#  if(length($x) >= $w) { return substr($x,0,$w); }
+#  return $x . $p x ($w-length($x));
+#}
+#
+##
+#sub isnum { my($x)=@_; return ($x=~ /^\s*(\+|\-)?(\d+\.?\d*|\d*\.?\d+)\s*$/); }
+def isnum(x):
+    '''Whether the argument is a valid real number.'''
+    return re.search(r'^\s*(\+|\-)?(\d+\.?\d*|\d*\.?\d+)\s*$', x)
+
+## round to nearest integer.
+#sub round1 { my($x) = @_; return int($x + .5 * ($x <=> 0)); }
+#
+#
+#
+## DATE/TIME FUNCTIONS FOLLOW
+#
+## Date/time: Takes unixtime in seconds and returns list of
+##   year, mon, day, hr, min, sec, day-of-week, day-of-year, is-daylight-time
+#sub dt { my($t) = @_;
+#  $t = time unless defined($t);
+#  my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($t);
+#  $year += 1900;  $mon = dd($mon+1);  $mday = dd($mday);
+#  $hour = dd($hour);  $min = dd($min); $sec = dd($sec);
+#  my %wh = ( 0=>"SUN",1=>"MON",2=>"TUE",3=>"WED",4=>"THU",5=>"FRI",6=>"SAT" );
+#  return ($year,$mon,$mday,$hour,$min,$sec,$wh{$wday},$yday,$isdst);
+#}
+#
+## Time string: takes unixtime and returns a formated YMD HMS string.
+#sub ts { my($t) = @_;
+#  my($year,$mon,$mday,$hour,$min,$sec,$wday,$yday,$isdst) = dt($t);
+#  return "$year-$mon-$mday $hour:$min:$sec $wday";
+#}
+#
+## Human-Compressed Time String: like 0711281947 for 2007-11-28 19:47
+#sub hcts { my($t) = @_;
+#  if($t % 60 >= 30) { $t += 60; } # round to the nearest minute.
+#  my($year,$mon,$mday,$hour,$min,$sec,$wday,$yday,$isdst) = dt($t);
+#  return substr($year,-2)."${mon}${mday}${hour}${min}";
+#}
+#
+## Seconds to str: takes number of seconds, returns a string like 1d02h03:04:05
+#sub ss { my($s) = @_;
+#  my($d,$h,$m);
+#  my $incl = "s";
+#
+#  if($s < 0) { return "-".ss(-$s); }
+#
+#  $m = int($s/60);
+#  if($m > 0) { $incl = "ms"; }
+#  $s %= 60;
+#  $h = int($m/60);
+#  if($h > 0) { $incl = "hms"; }
+#  $m %= 60;
+#  $d = int($h/24);
+#  if($d > 0) { $incl = "dhms"; }
+#  $h %= 24;
+#
+#  return ($incl=~"d" ? "$d"."d" : "").
+#         ($incl=~"h" ? dd($h)."h" : "").
+#         ($incl=~"m" ? dd($m).":" : "").
+#         ($incl!~"m" ? $s : dd($s))."s";
+#}
+#
+## just like above but with the biggest possible unit being hours instead of days
+#sub ss2 { my($s) = @_;
+#  my($d,$h,$m);
+#  my $incl = "s";
+#
+#  if($s < 0) { return "-".ss2(-$s); }
+#
+#  $m = int($s/60);
+#  if($m > 0) { $incl = "ms"; }
+#  $s %= 60;
+#  $h = int($m/60);
+#  if($h > 0) { $incl = "hms"; }
+#  $m %= 60;
+#
+#  return ($incl=~"h" ? $h."h" : "").
+#         ($incl=~"m" ? dd($m).":" : "").
+#         ($incl!~"m" ? $s : dd($s))."s";
+#}
+#
+#
+## Parse ss: takes a string like the one returned from ss() and parses it,
+## returning a number of seconds.
+#sub pss { my($s) = @_;
+#  $s =~ /^\s*(\-?)(\d*?)d?(\d*?)h?(\d*?)(?:\:|m)?(\d*?)s?\s*$/;
+#  return ($1 eq '-' ? -1 : 1) * ($2*24*3600+$3*3600+$4*60+$5);
+#}
+#
+## Parse Date: must be in year, month, day, hour, min, sec order, returns
+##   unixtime.
+#sub pd { my($s) = @_;
+#  my($year, $month, $day, $hour, $minute, $second);
+#
+#  if($s =~ m{^\s*(\d{1,4})\W*0*(\d{1,2})\W*0*(\d{1,2})\W*0*
+#                 (\d{0,2})\W*0*(\d{0,2})\W*0*(\d{0,2})\s*.*$}x) {
+#    $year = $1;  $month = $2;   $day = $3;
+#    $hour = $4;  $minute = $5;  $second = $6;
+#    $hour |= 0;  $minute |= 0;  $second |= 0;  # defaults.
+#    $year = ($year<100 ? ($year<70 ? 2000+$year : 1900+$year) : $year);
+#  }
+#  else {
+#    ($year,$month,$day,$hour,$minute,$second) =
+#      (1969,12,31,23,59,59); # indicates couldn't parse it.
+#  }
+#
+#  return timelocal($second,$minute,$hour,$day,$month-1,$year);
+#}
+#
+#1;  # perl wants this for libraries imported with 'require'.
+#
+#
+## SCRATCH AREA:
+#
+## Implementation of ran0 in C, from numerical recipes:
+#
+## #define IA 16807
+## #define IM 2147483647
+## #define AM (1.0/IM)
+## #define IQ 127773
+## #define IR 2836
+## static long seed = 1;
+## long ran0() {
+##   long k = (seed)/IQ;
+##   seed = IA*((seed) - k*IQ) - IR*k;
+##   if (seed < 0) { seed += IM; }
+##   return (seed);
+## }
+#
+## Implementation of ran0 in Mathematica:
+#
+## IA = 7^5;  IM = 2^31-1;
+## RAN = Rationalize[AbsoluteTime[]*1000,1];
+## setSeed[i_] := (RAN = i)
+## ran0[] := (RAN = Mod[IA * RAN, IM])
+#
+
+
+
+
